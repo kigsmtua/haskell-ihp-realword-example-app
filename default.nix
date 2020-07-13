b97836bf39449bc4c2ea4a194e6972006f5442c5
John Kiragu <mutuakiragu@gmail.com>
2020-07-13 17:25:35 +0300

    [Feat] Initial project setup

---
+++ default.nix
@@ -0,0 +1,22 @@
+let
+    ihp = builtins.fetchGit {
+        url = "https://github.com/digitallyinduced/ihp.git";
+        rev = "fb652b7f96278d3c4607123d3e37a6e0e9c6d5a7";
+    };
+    haskellEnv = import "${ihp}/NixSupport/default.nix" {
+        ihp = ihp;
+        haskellDeps = p: with p; [
+            cabal-install
+            base
+            wai
+            text
+            hlint
+            p.ihp
+        ];
+        otherDeps = p: with p; [
+            # Native dependencies, e.g. imagemagick
+        ];
+        projectPath = ./.;
+    };
+in
+    haskellEnv
